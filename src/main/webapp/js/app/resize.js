0794fd5fd85c9e6d25a0a5c5ab2c1725a59d16f2
Marian <dendrofit@mail.com>
2018-01-13 13:00:00 +0100

    january2018

---
+++ src/main/webapp/js/app/resize.js
@@ -0,0 +1,30 @@
+define(function () {
+    return  function (file, maxSize, callback) {
+        console.log('initial size=' + file.size);
+        var image = new Image();
+        image.onload = function ( ) {
+            URL.revokeObjectURL(this.src);
+            // Resize image
+            var canvas = document.createElement('canvas'),
+                    width = image.width,
+                    height = image.height;
+
+            if (width > height) {
+                if (width > maxSize) {
+                    height *= maxSize / width;
+                    width = maxSize;
+                }
+            } else {
+                if (height > maxSize) {
+                    width *= maxSize / height;
+                    height = maxSize;
+                }
+            }
+            canvas.width = width;
+            canvas.height = height;
+            canvas.getContext('2d').drawImage(image, 0, 0, width, height);
+            canvas.toBlob(callback, 'image/jpeg', 0.8);
+        };
+        image.src = URL.createObjectURL(file);
+    } ;
+});
--- src/main/webapp/js/app/sub.js
+++
@@ -1,6 +0,0 @@
-define(function () {
-    console.log('define sub');
-    return function print(msg) {
-        console.log(msg);
-    };
-});
